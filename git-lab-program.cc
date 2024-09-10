ddcd865e07f1a914f7ce3ab7e5d0fb46946bd7e0
victoriapalo <vp238423@ohio.edu>
2024-09-10 12:54:27 -0400

    Hello

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Victoria Palozzi
+ *        Date: September 9th, 2024
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
